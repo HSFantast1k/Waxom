49b351388024e6964cc296987bfb554ad6d6b8c2
Fantast1k <83510907+HSFantast1k@users.noreply.github.com>
2021-10-26 16:30:13 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -8,6 +8,14 @@ $(function(){
         prevArrow: '<button type="button" class="slick-arrows slick-prev"><img src="images/left.svg" alt="prev"></button>',
         nextArrow: '<button type="button" class="slick-arrows slick-next"><img src="images/right.svg" alt="next"></button>',
         fade: true,
+        responsive: [
+            {
+                breakpoint: 1101,
+                settings: {
+                    arrows: false,
+                }
+            }
+        ]
     });
 
     
@@ -26,6 +34,23 @@ $(function(){
         arrows: true,
         prevArrow: '<button type="button" class="slick-arrows slick-prev"><img src="images/post-arrow-left.svg" alt="prev"></button>',
         nextArrow: '<button type="button" class="slick-arrows slick-next"><img src="images/post-arrow-right.svg" alt="next"></button>',
+        responsive: [
+            {
+                breakpoint: 1241,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 651,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
     });
     
+    $(".menu__btn").on('click', function(){
+        $('.menu__list').toggleClass('menu__list--active');
+    });
 });
